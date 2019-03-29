ebaa9da5e5e62714d7f2ae6703427bb5d72e2a53
rifkegribenes <rifkegribenes@gmail.com>
2019-03-28 22:18:07 -0700

    many-to-many relationship items / orders

--- models/order.py
+++ models/order.py
@@ -3,13 +3,25 @@ from typing import List
 import os
 
 
+class ItemsInOrder(db.Model):
+    __tablename__ = "items_in_order"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
+    quantity = db.Column(db.Integer)
+
+    item = db.relationship("ItemModel")
+    order = db.relationship("OrderModel", back_populates="items")
+
+
 class OrderModel(db.Model):
     __tablename__ = "orders"
 
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(20), nullable=False)
 
-    items = db.relationship("ItemModel", lazy="dynamic")
+    items = db.relationship("ItemsInOrder", back_populates="order")
 
     @classmethod
     def find_all(cls) -> List["OrderModel"]:
